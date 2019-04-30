f58bda0c712cc22a4af99ccdce91f8c46595f3bf
hklee <hklee@yna.co.kr>
2019-04-30 13:12:11 +0900

    Completed separate apps by folder

---
+++ screens/build/precache-manifest.bfb57ba5feaa1e4d06c25124309e8793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68338f9ddd842b443bfba9b43eee3e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2a02861e83206a8a008c",
+    "url": "./static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "./static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "2a02861e83206a8a008c",
+    "url": "./static/js/main.1ea7e713.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
